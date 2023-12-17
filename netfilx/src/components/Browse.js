53c15b804b7ee88657f5a7aecb1aa0e6114bde3d
jotiraj123 <jotirajkpatil12@gmail.com>
2023-12-17 14:12:23 +0530

    work on login

--- netfilx/src/components/Browse.js
+++ netfilx/src/components/Browse.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Footer = () => {
+const Browse = () => {
   return (
     <div>
-      Footer
+      browser
     </div>
   )
 }
 
-export default Footer
+export default Browse
