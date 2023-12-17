f10ac9ac77fbd68743d3dc4dec58b0788932f34d
jotiraj123 <jotirajkpatil12@gmail.com>
2023-12-17 19:50:09 +0530

    setup router in netfilx project

--- netfilx/src/components/Body.js
+++ netfilx/src/components/Body.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Browse from './Browse'
 import Login from './Login'
+import {RouterProvider} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 const Body = () => {
+    const appRouter=createBrowserRouter([
+    {
+        path:'/',
+        element:<Login/>
+    },
+    {
+        path:"/browse",
+        element:<Browse/>
+    }
+])
   return (
+ 
     <div>
-      <Login />
-      <Browse/>
+    <RouterProvider router={appRouter} />
+ 
+    
     </div>
   )
 }
