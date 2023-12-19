e5a9e6af4eda93bf49aa6cab381713811c4050cc
jotiraj123 <jotirajkpatil12@gmail.com>
2023-12-19 23:13:06 +0530

    sign up and sign in page Done

--- netfilx/src/components/Login.js
+++ netfilx/src/components/Login.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+
+  const [isSignInForm , setSignInForm] = useState(true);
+  const toggleSignInForm=()=>{
+     setSignInForm(!isSignInForm)
+  }
   return (
     <div>
       <Header/>
@@ -11,17 +16,19 @@ const Login = () => {
       />
       </div>
       <form className=' w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0  text-white rounded-lg'>
-       <h1 className='font-bold text-3xl py-4'>Sign In</h1>
+       <h1 className='font-bold text-3xl py-4'>{isSignInForm ? 'Sign In':"Sign Up"}</h1>
         <input type='text' placeholder='Email Address'
-         className='p-2 my-2 w-full bg-gray-400' />
-
-<input type='password' placeholder='Password'
-         className='p-2 my-2 w-full'/>
+         className='p-2 my-2 w-full bg-gray-700' />
+   {!isSignInForm && (<input type='text' placeholder='Full Name'
+         className='p-2 my-2 w-full  bg-gray-700'/>)}
+   <input type='password' placeholder='Password'
+         className='p-2 my-2 w-full bg-gray-700'/>
 
         <button
          className='p-4 my-4
           bg-red-700
-         text-white w-full'>Sign In</button>
+         text-white w-full'>{isSignInForm ? 'Sign In':"Sign Up"}</button>
+         <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netfilx?  Sign up Now" : "Already Signed?  Sign In Now"}</p>
       </form>
      
     </div>
