6e3089387126ca37e2fae1a3cfc7c7be591d1062
jotiraj123 <jotirajkpatil12@gmail.com>
2023-12-19 23:32:29 +0530

    work in validate in sign up and sign in page

---
+++ netfilx/src/utils/validate.js
@@ -0,0 +1,8 @@
+export const checkValiData =(email, password)=>{
+    const isEmailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+    const isPasswordValid =/^[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password);
+if(!isEmailValid)return "Email Id is not vaild"
+if(!isPasswordValid) return "password is not valid"
+   
+return null;
+}
