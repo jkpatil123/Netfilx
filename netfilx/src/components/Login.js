6f17ea4ab7c50a6de781d292bc7143a45030613b
jotiraj123 <jotirajkpatil12@gmail.com>
2023-12-19 23:42:27 +0530

    Update Login.js

--- netfilx/src/components/Login.js
+++ netfilx/src/components/Login.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValiData} from '../utils/validate'
 
 const Login = () => {
+  const email = useRef(null);
+  const password= useRef(null);
+  const handleButtonClick =()=>{
+    // validate the form data
+     console.log(email);
+     console.log(password)
+  }
 
   const [isSignInForm , setSignInForm] = useState(true);
   const toggleSignInForm=()=>{
@@ -17,17 +25,17 @@ const Login = () => {
       </div>
       <form className=' w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0  text-white rounded-lg'>
        <h1 className='font-bold text-3xl py-4'>{isSignInForm ? 'Sign In':"Sign Up"}</h1>
-        <input type='text' placeholder='Email Address'
+        <input  ref={email}  type='text' placeholder='Email Address'
          className='p-2 my-2 w-full bg-gray-700' />
-   {!isSignInForm && (<input type='text' placeholder='Full Name'
+   {!isSignInForm && (<input ref={text}  type='text' placeholder='Full Name'
          className='p-2 my-2 w-full  bg-gray-700'/>)}
-   <input type='password' placeholder='Password'
+   <input ref={password}   type='password' placeholder='Password'
          className='p-2 my-2 w-full bg-gray-700'/>
 
         <button
          className='p-4 my-4
           bg-red-700
-         text-white w-full'>{isSignInForm ? 'Sign In':"Sign Up"}</button>
+         text-white w-full' onClick={handleButtonClick}>{isSignInForm ? 'Sign In':"Sign Up"}</button>
          <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netfilx?  Sign up Now" : "Already Signed?  Sign In Now"}</p>
       </form>
      
