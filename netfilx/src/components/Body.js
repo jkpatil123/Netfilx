336377853850d27237b9258b2516aedf0f64bb47
jotiraj123 <jotirajkpatil12@gmail.com>
2024-01-03 23:01:41 +0530

    working on netfilx website

--- netfilx/src/components/Body.js
+++ netfilx/src/components/Body.js
@@ -43,6 +43,7 @@ useEffect(() => {
   });
 
 }, []);
+
   return (
  
     <div>
--- netfilx/src/components/Browse.js
+++ netfilx/src/components/Browse.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Header from './Header'
 
 const Browse = () => {
   return (
     <div>
      <div>
-      browser
+      <Header />
     
-    <div>ffuf</div>
+
 
     </div>
 
